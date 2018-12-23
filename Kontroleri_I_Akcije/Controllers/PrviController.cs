f41639e11784b31b1b02efb96e54c54aeb94ef57
Rosana <rodjuga@gmail.com>
2018-12-23 23:32:54 +0100

    Add first Controller with string message

---
+++ Kontroleri_I_Akcije/Controllers/PrviController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kontroleri_I_Akcije.Controllers
+{
+    public class PrviController : Controller
+    {
+        // GET: Prvi
+        public string Pozdrav()
+        {
+            return "Pozdrav sa prvog kontrolera";
+        }
+    }
+}
