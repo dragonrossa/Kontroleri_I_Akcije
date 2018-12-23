ab399b2abe5b27859090fa4bed7725a5cec5a215
Rosana <rodjuga@gmail.com>
2018-12-23 23:45:42 +0100

    New method KontekstniPodaci

--- Kontroleri_I_Akcije/Controllers/PrviController.cs
+++ Kontroleri_I_Akcije/Controllers/PrviController.cs
@@ -13,5 +13,14 @@ namespace Kontroleri_I_Akcije.Controllers
         {
             return "Pozdrav sa prvog kontrolera";
         }
+
+
+        public string KontekstniPodaci()
+        {
+            string serverName = Server.MachineName;
+            DateTime datestamp = HttpContext.Timestamp;
+            return "<h2> Kontekstni podaci iz zahtjeva: <h2>" +
+                "<ul>" + "<li>" + serverName + "</li>" + "<li>" + datestamp.ToString() + "</li>" + "</ul>";
+        }
     }
 }
